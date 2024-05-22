b6bda9908014b385e25dd3adaaa9f57767939914
mmontan0 <165187154+mmontan0@users.noreply.github.com>
2024-05-22 02:43:55 +0000

    courses + departments

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,6 +1,7 @@
 class DepartmentsController < ApplicationController
   def index
     @departments = Department.all.order({ :created_at => :desc })
+  
 
     render({ :template => "departments/index" })
   end
@@ -8,6 +9,7 @@ class DepartmentsController < ApplicationController
   def show
     the_id = params.fetch("path_id")
     @department = Department.where({:id => the_id })
+    @the_department=@department.at(0)
 
     render({ :template => "departments/show" })
   end
